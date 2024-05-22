167108c2423e23c12f048f086d3252cf7a7b5175
Nestor Pena <nestor2509@gmail.com>
2024-05-22 07:03:28 -0500

    fix port frontend

--- local-machine/archihub/frontend/replace_vars.sh
+++ local-machine/archihub/frontend/replace_vars.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+echo "Replacing localhost:5000 with localhost:${FLASK_PORT} in all js files"
+
 find /usr/share/nginx/html/static/js -name "*.js" -exec sed -i "s|localhost:5000|localhost:${FLASK_PORT}|g" {} \;
 
 echo "Replaced localhost:5000 with localhost:${FLASK_PORT} in all js files"
