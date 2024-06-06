26e8cd88925b9e97e27a9b7ffca79cfa6915c0f8
Nestor Pena <nestor2509@gmail.com>
2024-06-06 10:39:38 -0500

    se agregan scripts generales para la administración de archihub

---
+++ scripts/folder_load.py
@@ -0,0 +1,115 @@
+import requests
+import json
+import datetime
+import os
+import argparse
+import dotenv
+
+dotenv.load_dotenv()
+
+key = os.getenv('ARCHIHUB_API_KEY')
+
+api_url = os.getenv('ARCHIHUB_API_URL')
+headers = {'Content-Type': 'application/json', 'Authorization': 'Bearer ' + key}
+
+def modify_dict(d, path, value):
+    keys = path.split('.')
+    for key in keys[:-1]:
+        d = d.setdefault(key, {})
+    d[keys[-1]] = value
+
+def get_resource(ident):
+    url = api_url + '/get_id'
+    r = requests.post(url, json={'ident': ident}, headers=headers)
+    if r.status_code == 200:
+        return json.loads(r.text)
+    else:
+        return None
+
+def cargar_carpeta(carpeta, parents = None, publish = False, default_type, main_type):
+    url = api_url + '/create'
+
+    # extraer el nombre de la carpeta de la ruta
+    nombre = carpeta.split('/')[-1]
+    # si la carpeta termina con /, se elimina
+    if nombre == '': nombre = carpeta.split('/')[-2]
+    # extraer solo la ruta de la carpeta
+    ruta = carpeta[:-len(nombre)]
+
+    # si existe el archivo nombre.txt en la carpeta, se lee el contenido y se guarda en type
+    try:
+        ruta = os.path.join(ruta, nombre + '.txt')
+        with open(ruta, 'r') as f:
+            type = f.read()
+    except:
+        type = default_type
+
+    payload = {}
+    modify_dict(payload, 'metadata.firstLevel.title', nombre)
+    modify_dict(payload, 'post_type', type)
+    payload['files'] = []
+
+    ident = datetime.datetime.now().strftime('%Y%m%d%H%M%S%f')
+    modify_dict(payload, 'ident', ident)
+
+    if parents:
+        payload['parents'] = parents
+        payload['parent'] = parents
+
+    if type != 'unidad-documental':
+        form = []
+        form.append(('data', (None, json.dumps(payload), 'application/json')))
+        r = requests.post(url, files=form, headers={'Authorization': 'Bearer ' + key})
+        print(r.json())
+        resource = get_resource(ident)
+        if resource:
+            parents = [resource]
+            num_children = len(os.listdir(carpeta))
+            folder_num = 0
+            files_num = 0
+            # iterar en las carpetas de la carpeta
+            for file_name in os.listdir(carpeta):
+                file_path = os.path.join(carpeta, file_name)
+                if os.path.isdir(file_path):
+                    folder_num += 1
+                    cargar_carpeta(file_path, parents)
+                else:
+                    if not file_name.endswith('.txt'):
+                        files_num += 1
+                        nombre = file_name.split('.')[0]
+                        payload = {}
+                        modify_dict(payload, 'metadata.firstLevel.title', nombre)
+                        modify_dict(payload, 'post_type', 'unidad-documental')
+                        modify_dict(payload, 'ident', datetime.datetime.now().strftime('%Y%m%d%H%M%S%f'))
+                        payload['parent'] = parents
+                        payload['parents'] = parents
+                        if publish:
+                            payload['status'] = 'published'
+
+                        file_path = os.path.join(carpeta, file_name)
+                        form = []
+                        form.append(('files', (file_name, open(file_path, 'rb'))))
+                        form.append(('data', (None, json.dumps(payload), 'application/json')))
+
+                        r = requests.post(url, data={**payload}, files=form, headers={'Authorization': 'Bearer ' + key})
+
+    else:
+        form = []
+        form.append(('data', (None, json.dumps(payload), 'application/json')))
+        for file_name in os.listdir(carpeta):
+            file_path = os.path.join(carpeta, file_name)
+            if os.path.isfile(file_path):
+                form.append(('files', (file_name, open(file_path, 'rb'))))
+
+        r = requests.post(url, data={**payload}, files=form, headers={'Authorization': 'Bearer ' + key})
+        print(r.json())
+
+parser = argparse.ArgumentParser(description='Load recursively a folder to Archihub')
+parser.add_argument('--folder', help='Folder to load recursively to Archihub', required=True)
+parser.add_argument('--default_type', help='Default type for the resources', required=True)
+parser.add_argument('--main_type', help='Main type for the resources', required=True)
+parser.add_argument('--publish', help='Publish the folder after loading', default=False)
+args = parser.parse_args()
+
+
+cargar_carpeta(args.carpeta, None, args.publish, args.default_type, args.main_type)
