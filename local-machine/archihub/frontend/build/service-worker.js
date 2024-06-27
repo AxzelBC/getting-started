d4d468a58ded31ac4dddd0c11c14d710bd53470b
Nestor Pena <nestor2509@gmail.com>
2024-06-27 08:06:14 -0500

    front con edición de transcripción

--- local-machine/archihub/frontend/build/service-worker.js
+++ local-machine/archihub/frontend/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.d3987464d0dfd48c5db7033f1ab45cc1.js"
+  "/precache-manifest.868e54a5fceb421f328989da774e1288.js"
 );
 
 self.addEventListener('message', (event) => {
